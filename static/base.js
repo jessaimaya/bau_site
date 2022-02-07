37ca622e3479566d5f92d49f98c448878a297dcf
jessai <maya.jessai@gmail.com>
2022-02-07 01:28:46 -0600

    feature: contact & resume

--- static/base.js
+++ static/base.js
@@ -23,20 +23,19 @@ const toggleNavbar = () => {
 };
 
 (function(){
-    window.onresize = () => draw();
     document.addEventListener("DOMContentLoaded", function() {
         toggleThemedElements();
-
-        window.fitText( document.getElementsByClassName("fit"), 0.6 );
-        window.fitText( document.getElementsByClassName("job__title"), 2 );
-        window.fitText( document.getElementsByClassName("job__details"), 2 );
-        window.fitText( document.getElementsByClassName("minfit"), 0.7 );
-        window.fitText( document.getElementsByClassName("minim"), 1.4 );
-        window.fitText( document.getElementById("skills"), 1.3 );
-        window.fitText( document.getElementById("experience"), 0.5 );
-        window.fitText( document.getElementById("mantra"), 2 );
-        window.fitText( document.getElementById("case_studies"), 1 );
-        draw();
+        if (window.fitText) {
+            window.fitText(document.getElementsByClassName("fit"), 0.6);
+            window.fitText(document.getElementsByClassName("job__title"), 2);
+            window.fitText(document.getElementsByClassName("job__details"), 2);
+            window.fitText(document.getElementsByClassName("minfit"), 0.7);
+            window.fitText(document.getElementsByClassName("minim"), 1.4);
+            window.fitText(document.getElementById("skills"), 1.3);
+            window.fitText(document.getElementById("experience"), 0.5);
+            window.fitText(document.getElementById("mantra"), 2);
+            window.fitText(document.getElementById("case_studies"), 1);
+        }
         /*
         AOS.init({
             duration: 800,
